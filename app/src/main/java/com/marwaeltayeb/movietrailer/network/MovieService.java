a453e6f5a2d612308172d8fb247a9125e1ffa172
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-09 21:43:50 +0200

    Create MovieService interface

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/network/MovieService.java
@@ -0,0 +1,18 @@
+package com.marwaeltayeb.movietrailer.network;
+
+import com.marwaeltayeb.movietrailer.models.MovieApiResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Marwa on 7/7/2019.
+ */
+
+public interface MovieService {
+
+    @GET("movie/popular")
+    Call<MovieApiResponse> getMovies(@Query("api_key") String apiKey, @Query("page") int page);
+
+}
