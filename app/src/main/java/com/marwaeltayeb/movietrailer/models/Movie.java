2db304f7fe3fe89b341cb45e3bae6f6c4e0b40a2
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-07-09 23:41:47 +0200

    Make Movie Attributes private

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/models/Movie.java
@@ -0,0 +1,56 @@
+package com.marwaeltayeb.movietrailer.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Movie {
+    @SerializedName("id")
+    public int movieId;
+    @SerializedName("vote_average")
+    private String movieVote;
+    @SerializedName("title")
+    private String movieTitle;
+    @SerializedName("poster_path")
+    private String moviePoster;
+    @SerializedName("overview")
+    private String movieDescription;
+    @SerializedName("release_date")
+    private String movieReleaseDate;
+    @SerializedName("original_language")
+    private String movieLanguage;
+    @SerializedName("genre_ids")
+    private List genreIds;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getMovieVote() {
+        return movieVote;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public String getMoviePoster() {
+        return moviePoster;
+    }
+
+    public String getMovieDescription() {
+        return movieDescription;
+    }
+
+    public String getMovieReleaseDate() {
+        return movieReleaseDate;
+    }
+
+    public String getMovieLanguage() {
+        return movieLanguage;
+    }
+
+    public List getGenreIds() {
+        return genreIds;
+    }
+}
