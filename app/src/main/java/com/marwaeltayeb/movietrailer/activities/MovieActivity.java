8bb62db786a78cd88d4991f6bb914c62cf88f7f0
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-06 21:45:11 +0200

    Return title and rating of movie

--- app/src/main/java/com/marwaeltayeb/movietrailer/activities/MovieActivity.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/MovieActivity.java
@@ -1,15 +1,37 @@
 package com.marwaeltayeb.movietrailer.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.marwaeltayeb.movietrailer.R;
 
+import static com.marwaeltayeb.movietrailer.Util.Constant.RATING_OF_MOVIE;
+import static com.marwaeltayeb.movietrailer.Util.Constant.TITLE_OF_MOVIE;
+
 public class MovieActivity extends AppCompatActivity {
+
+    ImageView backdropImage;
+    TextView movieTitle;
+    TextView movieRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        backdropImage = findViewById(R.id.backdropImage);
+        movieTitle = findViewById(R.id.titleOfMovie);
+        movieRating = findViewById(R.id.ratingOfMovie);
+
+        Intent intent = getIntent();
+        String titleOfMovie = intent.getStringExtra(TITLE_OF_MOVIE);
+        String ratingOfMovie = intent.getStringExtra(RATING_OF_MOVIE);
+        movieTitle.setText(titleOfMovie);
+        movieRating.setText(ratingOfMovie);
+
+
     }
 }
