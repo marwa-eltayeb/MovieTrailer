b467361cb4d73971a3e0dd565eb80379b25ac161
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-06 21:44:18 +0200

    Add Constant Variables

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/Util/Constant.java
@@ -0,0 +1,13 @@
+package com.marwaeltayeb.movietrailer.Util;
+
+/**
+ * Created by Marwa on 8/6/2019.
+ */
+
+public class Constant {
+
+    // Constant Variables
+    public static String TITLE_OF_MOVIE = "title";
+    public static String RATING_OF_MOVIE = "rating";
+
+}
