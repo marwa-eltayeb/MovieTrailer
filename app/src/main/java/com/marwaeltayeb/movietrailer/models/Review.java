b8fe8ef0e680c4469565ae7f41e330cb9dfed9be
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-07 23:08:17 +0200

    Create Review model

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/models/Review.java
@@ -0,0 +1,38 @@
+package com.marwaeltayeb.movietrailer.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Review {
+
+    @SerializedName("id")
+    private String id;
+    @SerializedName("author")
+    private String author;
+    @SerializedName("content")
+    private String content;
+    @SerializedName("url")
+    private String url;
+
+    public Review(String id, String author, String content, String url) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
