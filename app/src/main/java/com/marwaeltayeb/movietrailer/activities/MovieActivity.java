ec0345c4b986e8d74552dc8ce2ed2ab3bbf0b6d6
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-21 23:53:43 +0200

    Get Trailers and Reviews

--- app/src/main/java/com/marwaeltayeb/movietrailer/activities/MovieActivity.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/MovieActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.marwaeltayeb.movietrailer.R;
@@ -14,18 +15,28 @@ import com.marwaeltayeb.movietrailer.Util.Utility;
 import com.marwaeltayeb.movietrailer.adapters.ReviewAdapter;
 import com.marwaeltayeb.movietrailer.adapters.TrailerAdapter;
 import com.marwaeltayeb.movietrailer.models.Review;
+import com.marwaeltayeb.movietrailer.models.ReviewApiResponse;
 import com.marwaeltayeb.movietrailer.models.Trailer;
+import com.marwaeltayeb.movietrailer.models.TrailerApiResponse;
+import com.marwaeltayeb.movietrailer.network.RetrofitClient;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 import static com.marwaeltayeb.movietrailer.R.id.languageOfMovie;
+import static com.marwaeltayeb.movietrailer.R.id.listOfReviews;
 import static com.marwaeltayeb.movietrailer.Util.Constant.BACKDROP_OF_MOVIE;
 import static com.marwaeltayeb.movietrailer.Util.Constant.DESCRIPTION_OF_MOVIE;
+import static com.marwaeltayeb.movietrailer.Util.Constant.ID_OF_MOVIE;
 import static com.marwaeltayeb.movietrailer.Util.Constant.IMAGE_URL;
 import static com.marwaeltayeb.movietrailer.Util.Constant.LANGUAGE_OF_MOVIE;
 import static com.marwaeltayeb.movietrailer.Util.Constant.RATING_OF_MOVIE;
 import static com.marwaeltayeb.movietrailer.Util.Constant.RELEASE_DATE;
 import static com.marwaeltayeb.movietrailer.Util.Constant.TITLE_OF_MOVIE;
+import static com.marwaeltayeb.movietrailer.network.MovieService.API_KEY;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -42,6 +53,8 @@ public class MovieActivity extends AppCompatActivity {
     TrailerAdapter trailerAdapter;
     RecyclerView reviewsRecyclerView, trailersRecyclerView;
 
+    String idOfMovie;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +68,9 @@ public class MovieActivity extends AppCompatActivity {
         movieLanguage = findViewById(languageOfMovie);
 
         Intent intent = getIntent();
+
+        idOfMovie = intent.getStringExtra(ID_OF_MOVIE);
+        //Toast.makeText(this, idOfMovie + "", Toast.LENGTH_SHORT).show();
         String titleOfMovie = intent.getStringExtra(TITLE_OF_MOVIE);
         String ratingOfMovie = intent.getStringExtra(RATING_OF_MOVIE);
         String imageOfMovie = intent.getStringExtra(BACKDROP_OF_MOVIE);
@@ -66,7 +82,7 @@ public class MovieActivity extends AppCompatActivity {
         movieTitle.setText(titleOfMovie);
         movieRating.setText(ratingOfMovie);
         movieDescription.setText(descriptionOfMovie);
-        moveReleaseDate.setText(formattedDate  + " " + "|");
+        moveReleaseDate.setText(formattedDate + " " + "|");
         movieLanguage.setText(languageOfMovie);
 
         Glide.with(this)
@@ -74,23 +90,72 @@ public class MovieActivity extends AppCompatActivity {
                 .into(backdropImage);
 
 
+        //.setMovementMethod(new ScrollingMovementMethod());
+
+
         setupRecyclerViews();
+        getReviews();
+        getTrailers();
 
     }
 
     private void setupRecyclerViews() {
         // Trailers
         trailersRecyclerView = findViewById(R.id.listOfTrailers);
-        trailerAdapter = new TrailerAdapter(this,trailerList);
         trailersRecyclerView.setHasFixedSize(true);
         trailersRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        trailersRecyclerView.setAdapter(trailerAdapter);
 
         // Reviews
-        reviewsRecyclerView = findViewById(R.id.listOfReviews);
-        reviewAdapter = new ReviewAdapter(this,reviewList);
+        reviewsRecyclerView = findViewById(listOfReviews);
         reviewsRecyclerView.setHasFixedSize(true);
         reviewsRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        reviewsRecyclerView.setAdapter(reviewAdapter);
     }
+
+    //420818
+    //301528
+    private void getReviews() {
+        RetrofitClient.getInstance()
+                .getMovieService().getReviews(("420818"), API_KEY)
+                .enqueue(new Callback<ReviewApiResponse>() {
+                    @Override
+                    public void onResponse(Call<ReviewApiResponse> call, Response<ReviewApiResponse> response) {
+                        if (response.body() != null) {
+                            reviewList = response.body().getReviews();
+                            reviewAdapter = new ReviewAdapter(getApplicationContext(), reviewList);
+                            //Toast.makeText(MovieActivity.this, reviewList.size() + " ", Toast.LENGTH_SHORT).show();
+                        }
+                        reviewsRecyclerView.setAdapter(reviewAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<ReviewApiResponse> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "Failed to get Reviews", Toast.LENGTH_SHORT).show();
+                        t.printStackTrace();
+                    }
+                });
+    }
+
+    private void getTrailers() {
+        RetrofitClient.getInstance()
+                .getMovieService().getTrailers(("301528"), API_KEY)
+                .enqueue(new Callback<TrailerApiResponse>() {
+
+                    @Override
+                    public void onResponse(Call<TrailerApiResponse> call, Response<TrailerApiResponse> response) {
+                        if (response.body() != null) {
+                            trailerList = response.body().getTrailers();
+                            trailerAdapter = new TrailerAdapter(getApplicationContext(), trailerList);
+                            //Toast.makeText(MovieActivity.this, trailerList.size() + " ", Toast.LENGTH_SHORT).show();
+                        }
+                        trailersRecyclerView.setAdapter(trailerAdapter);
+                    }
+
+                    @Override
+                    public void onFailure(Call<TrailerApiResponse> call, Throwable t) {
+                        Toast.makeText(getApplicationContext(), "Failed to get Trailers", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
 }
