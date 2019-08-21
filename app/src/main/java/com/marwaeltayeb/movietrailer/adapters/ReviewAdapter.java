d8d2f14098758cc77a711ea9d5a708a94cbf033b
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-22 00:27:28 +0200

    Add setOnClickListener for showing review

--- app/src/main/java/com/marwaeltayeb/movietrailer/adapters/ReviewAdapter.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/adapters/ReviewAdapter.java
@@ -1,6 +1,7 @@
 package com.marwaeltayeb.movietrailer.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.marwaeltayeb.movietrailer.R;
+import com.marwaeltayeb.movietrailer.activities.WebViewActivity;
 import com.marwaeltayeb.movietrailer.models.Review;
 
 import java.util.List;
@@ -18,6 +20,7 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewView
     private Context mContext;
     // Declare an arrayList for trailers
     private List<Review> reviewList;
+    private Review currentReview;
 
     public ReviewAdapter(Context mContext, List<Review> reviewList) {
         this.mContext = mContext;
@@ -33,10 +36,10 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewView
 
     @Override
     public void onBindViewHolder(@NonNull ReviewViewHolder holder, int position) {
-        Review currentReview = reviewList.get(position);
+        currentReview = reviewList.get(position);
         holder.reviewOfMovie.setText(currentReview.getContent());
         holder.authorOfReview.setText(currentReview.getAuthor());
-        holder.urlOfReview.setText(currentReview.getUrl());
+        //holder.urlOfReview.setText(currentReview.getUrl());
     }
 
     @Override
@@ -59,7 +62,20 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ReviewView
             reviewOfMovie = itemView.findViewById(R.id.reviewOfMovie);
             authorOfReview = itemView.findViewById(R.id.authorOfReview);
             urlOfReview = itemView.findViewById(R.id.urlOfReview);
+
+            urlOfReview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), WebViewActivity.class);
+                    String url = currentReview.getUrl();
+                    intent.putExtra("url", url);
+                    v.getContext().startActivity(intent);
+
+                }
+            });
         }
+
+
     }
 
 
