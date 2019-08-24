d770bf5bb5547d1b62bf3660aba05c22b8b44b75
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-08-25 00:53:02 +0200

    Add ScnackBar

--- app/src/main/java/com/marwaeltayeb/movietrailer/activities/MainActivity.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
@@ -23,6 +24,7 @@ import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
@@ -104,6 +106,8 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         // Register the listener
         PreferenceManager.getDefaultSharedPreferences(this)
                 .registerOnSharedPreferenceChangeListener(this);
+
+
     }
 
 
@@ -229,7 +233,16 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
             return true;
         } else {
             progressDialog.dismiss();
-            Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG).show();
+            Snackbar.make(findViewById(android.R.id.content), "No Internet Connection", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("CLOSE", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                        }
+                    })
+                    .setActionTextColor(getResources().getColor(R.color.colorAccent))
+                    .show();
             return false;
         }
     }
