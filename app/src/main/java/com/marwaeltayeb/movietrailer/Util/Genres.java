ea3e7856d691b7c0fdaa64bb075e24bd793355f6
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-09-12 00:35:45 +0200

    Create class for genres

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/Util/Genres.java
@@ -0,0 +1,32 @@
+package com.marwaeltayeb.movietrailer.Util;
+
+import java.util.HashMap;
+
+public class Genres {
+
+    private static final HashMap<Integer, String> genres = new HashMap<Integer, String>() {{
+        put(28, "Action");
+        put(12, "Adventure");
+        put(16, "Animation");
+        put(35, "Comedy");
+        put(80, "Crime");
+        put(99, "Documentary");
+        put(18, "Drama");
+        put(10751, "Family");
+        put(14, "Fantasy");
+        put(36, "History");
+        put(27, "Horror");
+        put(10402, "Music");
+        put(9648, "Mystery");
+        put(10749, "Romance");
+        put(878, "Science Fiction");
+        put(10770, "TV Movie");
+        put(53, "Thriller");
+        put(10752, "War");
+        put(37, "Western");
+    }};
+
+    public static HashMap<Integer, String> getGenres() {
+        return genres;
+    }
+}
