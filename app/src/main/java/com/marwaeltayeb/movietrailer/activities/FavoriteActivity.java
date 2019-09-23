38f6f6aeffb6f25d60d6a7c65e0ab7b68791d8b3
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-09-23 23:11:19 +0200

    Add FavoriteActivity

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/FavoriteActivity.java
@@ -0,0 +1,15 @@
+package com.marwaeltayeb.movietrailer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marwaeltayeb.movietrailer.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
