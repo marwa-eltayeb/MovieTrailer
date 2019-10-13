16e5fef96a3503dd7b08039b3525ab282c7582aa
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-10-13 22:58:03 +0200

    Add a Room database

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/database/MovieRoomDatabase.java
@@ -0,0 +1,32 @@
+package com.marwaeltayeb.movietrailer.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {MovieEntry.class}, version = 1, exportSchema = false)
+public abstract class MovieRoomDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "movie_database";
+
+    private static MovieRoomDatabase sInstance;
+
+    static MovieRoomDatabase getDatabase(final Context context) {
+        if (sInstance == null) {
+            synchronized (MovieRoomDatabase.class) {
+                if (sInstance == null) {
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            MovieRoomDatabase.class, DATABASE_NAME)
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public abstract MovieDao movieDao();
+
+}
+
