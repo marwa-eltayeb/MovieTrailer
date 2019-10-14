bcb1d76703af893fc871eb9f1ae578f84f756282
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-10-15 01:10:43 +0200

    Create FavoriteAdapter

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/adapters/FavoriteAdapter.java
@@ -0,0 +1,119 @@
+package com.marwaeltayeb.movietrailer.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.marwaeltayeb.movietrailer.R;
+import com.marwaeltayeb.movietrailer.database.MovieEntry;
+
+public class FavoriteAdapter extends ListAdapter<MovieEntry, FavoriteAdapter.FavoriteHolder> {
+
+    private Context mContext;
+    private MovieEntry favoriteMovie;
+
+    // Create a final private FavoriteAdapterOnClickHandler called mClickHandler
+    private FavoriteAdapter.FavoriteAdapterOnClickHandler clickHandler;
+
+    /**
+     * The interface that receives onClick messages.
+     */
+    public interface FavoriteAdapterOnClickHandler {
+        void onClick(MovieEntry movieEntry);
+    }
+
+    public FavoriteAdapter(Context mContext, FavoriteAdapter.FavoriteAdapterOnClickHandler clickHandler) {
+        super(DIFF_CALLBACK);
+        this.mContext = mContext;
+        this.clickHandler = clickHandler;
+    }
+
+    private static final DiffUtil.ItemCallback<MovieEntry> DIFF_CALLBACK = new DiffUtil.ItemCallback<MovieEntry>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull MovieEntry oldItem, @NonNull MovieEntry newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull MovieEntry oldItem, @NonNull MovieEntry newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle()) &&
+                    oldItem.getDescription().equals(newItem.getDescription());
+        }
+    };
+
+    @NonNull
+    @Override
+    public FavoriteHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+        return new FavoriteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteHolder holder, int position) {
+        favoriteMovie = getItem(position);
+
+        if (favoriteMovie != null) {
+            holder.movieTitle.setText(favoriteMovie.getTitle());
+            holder.movieRating.setText(favoriteMovie.getVote());
+
+            RequestOptions options = new RequestOptions()
+                    .centerCrop()
+                    .placeholder(R.drawable.maze_ruuner)
+                    .error(R.drawable.dots)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .priority(Priority.HIGH)
+                    .dontAnimate()
+                    .dontTransform();
+
+            // Load the Movie poster into ImageView
+            /*
+            Glide.with(mContext)
+                    .load(IMAGE_URL + favoriteMovie.getPoster())
+                    //.apply(options)
+                    .into(holder.moviePoster);
+            */
+        }
+    }
+
+    public MovieEntry getMovieAt(int position) {
+        return getItem(position);
+    }
+
+    class FavoriteHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        // Create view instances
+        TextView movieTitle;
+        TextView movieRating;
+        ImageView moviePoster;
+
+        public FavoriteHolder(View itemView) {
+            super(itemView);
+            movieTitle = itemView.findViewById(R.id.movie_title);
+            movieRating = itemView.findViewById(R.id.movie_rating);
+            moviePoster = itemView.findViewById(R.id.movie_poster);
+            // Register a callback to be invoked when this view is clicked.
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            // Get position of the movie
+            favoriteMovie = getItem(position);
+            // Send movie through click
+            clickHandler.onClick(favoriteMovie);
+        }
+    }
+
+
+
+}
