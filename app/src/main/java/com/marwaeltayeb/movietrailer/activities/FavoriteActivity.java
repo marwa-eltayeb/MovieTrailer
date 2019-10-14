93c1448c2cf20088eea4d8962d1be40347aace1a
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-10-15 01:11:04 +0200

    Read favorite movies

--- app/src/main/java/com/marwaeltayeb/movietrailer/activities/FavoriteActivity.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/FavoriteActivity.java
@@ -1,15 +1,77 @@
 package com.marwaeltayeb.movietrailer.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.marwaeltayeb.movietrailer.R;
+import com.marwaeltayeb.movietrailer.adapters.FavoriteAdapter;
+import com.marwaeltayeb.movietrailer.database.MovieEntry;
+import com.marwaeltayeb.movietrailer.database.MovieRoomViewModel;
 
-public class FavoriteActivity extends AppCompatActivity {
+import java.util.List;
+
+import static com.marwaeltayeb.movietrailer.utils.Constant.MOVIE;
+
+public class FavoriteActivity extends AppCompatActivity implements FavoriteAdapter.FavoriteAdapterOnClickHandler{
+
+    private RecyclerView recyclerView;
+    private FavoriteAdapter favoriteAdapter;
+    private MovieRoomViewModel mMovieRoomViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        // Set up recyclerView
+        recyclerView = findViewById(R.id.favorite_list);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) ? 3 : 4));
+        recyclerView.setHasFixedSize(true);
+
+        favoriteAdapter = new FavoriteAdapter(this,this);
+        mMovieRoomViewModel = ViewModelProviders.of(this).get(MovieRoomViewModel.class);
+
+        loadMoviesFromDatabase();
+
+    }
+
+    /**
+     * Load movies from database
+     */
+    private void loadMoviesFromDatabase() {
+        // Observe the movieList from ViewModel
+        mMovieRoomViewModel.getAllFavoriteMovies().observe(this, new Observer<List<MovieEntry>>() {
+            @Override
+            public void onChanged(@Nullable final List<MovieEntry> favoriteMovies) {
+                // Update the cached copy of the movies in the adapter.
+                favoriteAdapter.submitList(favoriteMovies);
+                //if(movieAdapter.getCurrentList() != null) {
+                    Log.v("favoriteList", favoriteMovies.size() + "");
+                    Toast.makeText(FavoriteActivity.this, favoriteMovies.size() + "", Toast.LENGTH_SHORT).show();
+                //}
+            }
+        });
+
+        // Set the adapter
+        recyclerView.setAdapter(favoriteAdapter);
+        favoriteAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(MovieEntry movieEntry) {
+        Intent intent = new Intent(FavoriteActivity.this, MovieActivity.class);
+        // Pass an object of movie class
+        intent.putExtra(MOVIE, (movieEntry));
+        startActivity(intent);
     }
 }
+
