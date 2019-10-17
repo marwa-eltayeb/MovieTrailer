17269e5acf815ca22f522e90056294fd738b63f8
Marwa-Said <marwa_eltayeb@yahoo.com>
2019-10-17 03:37:26 +0200

    Insert only one object

--- app/src/main/java/com/marwaeltayeb/movietrailer/database/MovieDao.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/database/MovieDao.java
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.marwaeltayeb.movietrailer.models.Movie;
@@ -13,7 +14,7 @@ import java.util.List;
 @Dao
 public interface MovieDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Movie movie);
 
     @Delete
