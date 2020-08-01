2b542587d3cbbb08e32f65c81ad2ecb219e36220
Marwa-Said <marwa_eltayeb@yahoo.com>
2020-08-01 22:31:29 +0200

    Create AboutActivity

---
+++ app/src/main/java/com/marwaeltayeb/movietrailer/activities/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.marwaeltayeb.movietrailer.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.marwaeltayeb.movietrailer.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
