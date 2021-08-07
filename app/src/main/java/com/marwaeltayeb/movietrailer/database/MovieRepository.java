6f1d5f98e790c99c3b66248d75b5ebca8d6032a8
Marwa-Said <marwa_eltayeb@yahoo.com>
2021-08-07 16:18:26 +0200

    Convert AsyncTask to ExecutorService

--- app/src/main/java/com/marwaeltayeb/movietrailer/database/MovieRepository.java
+++ app/src/main/java/com/marwaeltayeb/movietrailer/database/MovieRepository.java
@@ -1,81 +1,44 @@
 package com.marwaeltayeb.movietrailer.database;
 
-import android.os.AsyncTask;
-
 import androidx.lifecycle.LiveData;
 
 import com.marwaeltayeb.movietrailer.models.Movie;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.inject.Inject;
 
 public class MovieRepository {
 
     private final MovieDao mMovieDao;
-    private final LiveData<List<Movie>> mAllMovies;
+    private static final int NUMBER_OF_THREADS = Runtime.getRuntime().availableProcessors();
+    private final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
+    @Inject
     public MovieRepository(MovieDao mMovieDao) {
         this.mMovieDao = mMovieDao;
-        mAllMovies = mMovieDao.getAllMovies();
     }
 
     LiveData<List<Movie>> getAllMovies() {
-        return mAllMovies;
+        return mMovieDao.getAllMovies();
     }
 
     public void insert(Movie movie) {
-        new InsertAsyncTask(mMovieDao).execute(movie);
-    }
-
-    private static class InsertAsyncTask extends AsyncTask<Movie, Void, Void> {
-
-        private MovieDao mAsyncTaskDao;
-
-        InsertAsyncTask(MovieDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(final Movie... params) {
-            mAsyncTaskDao.insert(params[0]);
-            return null;
-        }
+        databaseWriteExecutor.execute(() -> {
+            mMovieDao.insert(movie);
+        });
     }
 
     public void deleteById(int movieId) {
-        new DeleteByIdAsyncTask(mMovieDao).execute(movieId);
-    }
-
-    private static class DeleteByIdAsyncTask extends AsyncTask<Integer, Void, Void> {
-
-        private MovieDao mAsyncTaskDao;
-
-        DeleteByIdAsyncTask(MovieDao dao) {
-            mAsyncTaskDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(final Integer... params) {
-            mAsyncTaskDao.deleteById(params[0]);
-            return null;
-        }
+        databaseWriteExecutor.execute(() -> {
+            mMovieDao.deleteById(movieId);
+        });
     }
 
     public void deleteAll(){
-        new DeleteAllAsyncTask(mMovieDao).execute();
-    }
-
-    private static class DeleteAllAsyncTask extends AsyncTask<Void,Void,Void>{
-
-        private MovieDao mAsyncTaskDao;
-
-        private DeleteAllAsyncTask(MovieDao deo){
-            this.mAsyncTaskDao = deo;
-        }
-
-        @Override
-        protected Void doInBackground(Void...v) {
-            mAsyncTaskDao.deleteAll();
-            return null;
-        }
+        databaseWriteExecutor.execute(mMovieDao::deleteAll);
     }
 }
